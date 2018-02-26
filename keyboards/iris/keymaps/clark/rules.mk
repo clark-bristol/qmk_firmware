c9c65352f9bb75a6976440f2f4e3e8dd352947dd
Clark Bristol <clark.bristol@ezcater.com>
2018-02-26 12:38:40 -0500

    Prevents the keyboard from going into command mode when you hold both Shift keys

--- keyboards/iris/keymaps/clark/rules.mk
+++ keyboards/iris/keymaps/clark/rules.mk
@@ -3,6 +3,9 @@ BACKLIGHT_ENABLE = yes
 # TAP_DANCE_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 
+# Commands for debug and configuration
+COMMAND_ENABLE   = no
+
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
 endif
