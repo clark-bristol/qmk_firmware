ead294783350975fc317ee461dd0c3df4e76b9e1
Clark Bristol <clark.bristol@ezcater.com>
2018-02-27 17:28:59 -0500

    Adds toggle of auto-shift

--- keyboards/iris/keymaps/clark/keymap.c
+++ keyboards/iris/keymaps/clark/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------|
      KC_LCTL , KC_A   ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,                         KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------.    ,--------|--------+--------+--------+--------+--------+--------|
-      KC_LSPO, KC_Z   ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  , KC_MEH,   TG(_RAISE),  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSPC,
+      KC_LSPO, KC_Z   ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,TG(_RAISE),    KC_ASTG,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSPC,
   //`--------+--------+--------+--------+--------+--------+--------/    \--------+--------+--------+--------+--------+--------+--------'
                                         KC_LALT, KC_LGUI, KC_SPC ,         KC_ENT , KC_LOWR, KC_LEAD
   //                                  `--------+--------+--------'        `--------+--------+--------'
