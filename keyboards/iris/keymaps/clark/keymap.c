3dc7b1f39478c810f5ab40031ddeb1bf8143b05b
Clark Bristol <clark.bristol@ezcater.com>
2018-04-07 08:28:43 -0400

    Attempt at Tapdancing Capslock Key

--- keyboards/iris/keymaps/clark/keymap.c
+++ keyboards/iris/keymaps/clark/keymap.c
@@ -16,15 +16,10 @@ enum custom_keycodes {
   ADJUST,
 };
 
-// tapdance definitions
-// enum {
-//   W_LPRN = 0,
-//   E_LBRC = 0,
-//   R_LCBR = 0,
-//   // TD_LALT,
-//   // TD_LSFT,
-//   // TD_RSFT,
-// };
+tapdance definitions
+enum {
+  CUST_CL = 0,
+};
 
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
@@ -40,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = KEYMAP(
   //,--------+--------+--------+--------+--------+--------.                      ,--------+--------+--------+--------+--------+--------.
-     KC_GESC , KC_1   ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                         KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_BSPC,
+     KC_GRAVE, KC_1   ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                         KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------|
      KC_TAB  , KC_Q   ,  KC_W  ,  KC_E  ,  KC_R  ,  KC_T  ,                         KC_Y  ,  KC_U  ,  KC_I  ,  KC_O  ,  KC_P  , KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------|
-     KC_LCTL , KC_A   ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,                         KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,
+     CUST_CL , KC_A   ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,                         KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------.    ,--------|--------+--------+--------+--------+--------+--------|
       KC_LSPO, KC_Z   ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  , KC_LEAD ,    KC_ASTG ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSPC,
   //`--------+--------+--------+--------+--------+--------+--------/    \--------+--------+--------+--------+--------+--------+--------'
@@ -106,142 +101,74 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-//Tap Dance Declarations
-//**************** Definitions needed for quad function to work *********************//
-//Enums used to clearly convey the state of the tap dance
+// Tap Dance Declarations
+// **************** Definitions needed for quad function to work *********************//
+// Enums used to clearly convey the state of the tap dance
 
-// enum {
-//   SINGLE_TAP = 1,
-//   SINGLE_HOLD = 2,
-//   DOUBLE_TAP = 3,
-//   DOUBLE_HOLD = 4,
-//   DOUBLE_SINGLE_TAP = 5 //send SINGLE_TAP twice - NOT DOUBLE_TAP
-//   // Add more enums here if you want for triple, quadruple, etc.
-// };
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5 //send SINGLE_TAP twice - NOT DOUBLE_TAP
+  // Add more enums here if you want for triple, quadruple, etc.
+};
 
-// typedef struct {
-//   bool is_press_action;
-//   int state;
-// } tap;
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
 
-// int cur_dance (qk_tap_dance_state_t *state) {
-//   if (state->count == 1) {
-//     //If count = 1, and it has been interrupted - it doesn't matter if it is pressed or not: Send SINGLE_TAP
-//     if (state->interrupted || state->pressed==0) return SINGLE_TAP;
-//     else return SINGLE_HOLD;
-//   }
-//   //If count = 2, and it has been interrupted - assume that user is trying to type the letter associated
-//   //with single tap. In example below, that means to send `xx` instead of `Escape`.
-//   else if (state->count == 2) {
-//     if (state->interrupted) return DOUBLE_SINGLE_TAP;
-//     else if (state->pressed) return DOUBLE_HOLD;
-//     else return DOUBLE_TAP;
-//   }
-//   else return 6; //magic number. At some point this method will expand to work for more presses
-// }
-
-
-
-//**************** Definitions needed for quad function to work *********************//
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    //If count = 1, and it has been interrupted - it doesn't matter if it is pressed or not: Send SINGLE_TAP
+    if (state->interrupted || state->pressed==0) return SINGLE_TAP;
+    else return SINGLE_HOLD;
+  }
+  //If count = 2, and it has been interrupted - assume that user is trying to type the letter associated
+  //with single tap. In example below, that means to send `xx` instead of `Escape`.
+  else if (state->count == 2) {
+    if (state->interrupted) return DOUBLE_SINGLE_TAP;
+    else if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  else return 6; //magic number. At some point this method will expand to work for more presses
+}
 
 
 
-// //LALT
-// static tap lalt_tap_state = {
-//   .is_press_action = true,
-//   .state = 0
-// };
-
-// void lalt_finished (qk_tap_dance_state_t *state, void *user_data) {
-//   lalt_tap_state.state = cur_dance(state);
-//   switch (lalt_tap_state.state) {
-//     case SINGLE_TAP: register_code(KC_LBRC); break;
-//     case SINGLE_HOLD: register_code(KC_LALT); break;
-//   }
-// }
-
-// void lalt_reset (qk_tap_dance_state_t *state, void *user_data) {
-//   switch (lalt_tap_state.state) {
-//     case SINGLE_TAP: unregister_code(KC_LBRC); break;
-//     case SINGLE_HOLD: unregister_code(KC_LALT); break;
-//   }
-//   lalt_tap_state.state = 0;
-// }
+// **************** Definitions needed for quad function to work *********************//
 
 
 
-// //LSFT
-// static tap lsft_tap_state = {
-//   .is_press_action = true,
-//   .state = 0
-// };
+//Capslock --> Tap: ESC, Hold: LCTL
+static tap cl_tap_state = {
+  .is_press_action = true,
+  .state = 0
+};
 
-// void lsft_finished (qk_tap_dance_state_t *state, void *user_data) {
-//   lsft_tap_state.state = cur_dance(state);
-//   switch (lsft_tap_state.state) {
-//     case SINGLE_TAP:
-//       register_code(KC_LSFT);
-//       register_code(KC_9);
-//       break;
-//     case SINGLE_HOLD: register_code(KC_LSFT); break;
-//   }
-// }
+void cl_finished (qk_tap_dance_state_t *state, void *user_data) {
+  cl_tap_state.state = cur_dance(state);
+  switch (cl_tap_state.state) {
+    case SINGLE_TAP: register_code(KC_ESC); break;
+    case SINGLE_HOLD: register_code(KC_LCTL); break;
+  }
+}
 
-// void lsft_reset (qk_tap_dance_state_t *state, void *user_data) {
-//   switch (lsft_tap_state.state) {
-//     case SINGLE_TAP:
-//       unregister_code(KC_9);
-//       unregister_code(KC_LSFT);
-//       break;
-//     case SINGLE_HOLD: unregister_code(KC_LSFT); break;
-//   }
-//   lsft_tap_state.state = 0;
-// }
+void cl_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (lalt_tap_state.state) {
+    case SINGLE_TAP: unregister_code(KC_ESC); break;
+    case SINGLE_HOLD: unregister_code(KC_LCTL); break;
+  }
+  lalt_tap_state.state = 0;
+}
 
 
 
-// // W
-// static tap w_tap_state = {
-//   .is_press_action = true,
-//   .state = 0
-// };
-
-// void w_finished (qk_tap_dance_state_t *state, void *user_data) {
-//   w_tap_state.state = cur_dance(state);
-//   switch (w_tap_state.state) {
-//     case SINGLE_TAP:
-//       register_code(KC_W);
-//       break;
-//     case DOUBLE_TAP:
-//       register_code(KC_LSFT);
-//       register_code(KC_9);
-//       break;
-//   }
-// }
-
-// void w_reset (qk_tap_dance_state_t *state, void *user_data) {
-//   switch (w_tap_state.state) {
-//     case SINGLE_TAP:
-//       unregister_code(KC_W);
-//       break;
-//     case DOUBLE_TAP:
-//       unregister_code(KC_9);
-//       unregister_code(KC_LSFT);
-//       break;
-//   }
-//   w_tap_state.state = 0;
-// }
-
-
-
-// //Tap Dance Definitions
-// qk_tap_dance_action_t tap_dance_actions[] = {
-//   // [W_LPRN]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, w_finished, w_reset),
-//   // [E_LBRC]  = ACTION_TAP_DANCE_DOUBLE (KC_E, KC_LBRC),
-//   // [R_LCBR]  = ACTION_TAP_DANCE_DOUBLE (KC_R, KC_LCBR),
-//   // [TD_LSFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lsft_finished, lsft_reset),
-//   // [TD_RSFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rsft_finished, rsft_reset),
-// };
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [CUST_CL]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, cl_finished, cl_reset),
+};
 
 
 
