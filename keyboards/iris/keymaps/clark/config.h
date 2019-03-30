8c88ec23d901fda450a04418e47106e3bab5ce06
Clark Bristol <clark.bristol@ezcater.com>
2019-03-30 17:07:05 -0400

    iterate and add gaming layer

--- keyboards/iris/keymaps/clark/config.h
+++ keyboards/iris/keymaps/clark/config.h
@@ -39,7 +39,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 
 // for Leader Key
-#define LEADER_TIMEOUT 500
+#define LEADER_TIMEOUT 300
+#define LEADER_PER_KEY_TIMING
 
 // for tapdance
 #define TAPPING_TERM 100
--- keyboards/iris/keymaps/clark/rules.mk
+++ keyboards/iris/keymaps/clark/rules.mk
@@ -2,6 +2,7 @@ RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 AUTO_SHIFT_ENABLE = no
+LEADER_ENABLE = yes
 
 # Commands for debug and configuration
 COMMAND_ENABLE   = no
