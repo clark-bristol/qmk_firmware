9d841f995f7b668a5e29b11b974c408156665032
Clark <clark.bristol@ezcater.com>
2020-01-12 14:06:35 -0500

    Change backspace to esc on planck gaming layer

--- keyboards/planck/keymaps/clark/keymap.c
+++ keyboards/planck/keymaps/clark/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_GAME] = LAYOUT_planck_grid(
-    KC_TAB  , KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB  , KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC,
     KC_LCTL , KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT , KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_GRAVE, KC_CAPS, KC_LALT, KC_LGUI, RAISE,  KC_SPC,  KC_SPC,  LOWER,   KC_RGUI, KC_RALT, KC_LEAD, KC_BSLS
