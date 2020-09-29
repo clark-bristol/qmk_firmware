e9142a104decf8c38d3b7fc1a30286192ef53832
Clark <clark.bristol@ezcater.com>
2020-09-28 21:36:33 -0400

    Make the clark iris keymap fully match the clark planck keymap

--- keyboards/keebio/iris/keymaps/clark/rules.mk
+++ keyboards/keebio/iris/keymaps/clark/rules.mk
@@ -3,6 +3,8 @@ BACKLIGHT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 AUTO_SHIFT_ENABLE = no
 LEADER_ENABLE = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+BOOTMAGIC_ENABLE = no
 
 # Commands for debug and configuration
 COMMAND_ENABLE   = no
